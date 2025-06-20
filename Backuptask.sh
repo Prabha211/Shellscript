f5f36e999d7636fdfda7fa73198cc196170aff62
Prabha211 <prabha.hegde@gmail.com>
2025-06-20 23:21:19 +0530

    Few lines added to Original file

--- Backuptask.sh
+++ Backuptask.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+Source="/c/Users/prabh/Tasks/Original.txt"
+Destination="/c/Users/prabh/Tasks/Day1/Backup.txt"
+cp "$Source" "$Destination"
+#echo "Backup completed: $Source to $Destination"
+echo "Backup completed Original.txt to Backup.txt"
+
+
